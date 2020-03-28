561ae97d73e1cd15aa2dc65ab787103489f306f3
Aminuddin <aminuddinpsg@gmail.com>
2020-03-28 12:11:47 +0800

    [react.js basics]static react component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+    </form>
+  );
+}
+
+const FilterableProductTable = () => {
+  return (
+    <div className="FilterableProductTable">
+      <SearchBar/>
+      <ProductTable/>
+    </div>
+  );
+}
+
+const ProductTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+    <tbody>
+      <ProductCategoryRow/>
+      <ProductRow/>
+    </tbody>
+  </table>
+  );
+}
+
+const ProductCategoryRow  = () => {
+  return (
+    <tr>
+      <th>
+        Price
+      </th>
+    </tr>
+  );
+
+}
+
+const ProductRow  = () => {
+  return (
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+    </tr>
+  );
+}
+
+ReactDOM.render(<FilterableProductTable/>, document.getElementById('root'));
