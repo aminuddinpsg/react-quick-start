4900bb79d74c6dfa503d0234c2b1b25b21f67ff2
Aminuddin Abdullah <amin@Aminuddins-MacBook-Air.local>
2020-03-28 13:06:58 +0800

    [react-basics] update static elements

--- src/index.js
+++ src/index.js
@@ -19,6 +19,18 @@ const FilterableProductTable = () => {
 }
 
 const ProductTable = () => {
+  const rows =[]
+  let lastCategory = null
+
+  PRODUCTS.forEach(
+    e => {
+      if(lastCategory != e.category)
+        rows.push(<ProductCategoryRow category={e.category} key={e.category}/>)
+
+      rows.push(<ProductRow product={e} key={e.name}/>)
+      lastCategory = e.category;
+    });
+
   return (
     <table>
       <thead>
@@ -27,32 +39,39 @@ const ProductTable = () => {
           <th>Price</th>
         </tr>
       </thead>
-    <tbody>
-      <ProductCategoryRow/>
-      <ProductRow/>
-    </tbody>
+    <tbody>{rows}</tbody>
   </table>
   );
 }
 
-const ProductCategoryRow  = () => {
+const ProductCategoryRow  = (props) => {
   return (
     <tr>
       <th>
-        Price
+        {props.category}
       </th>
     </tr>
   );
 
 }
 
-const ProductRow  = () => {
+const ProductRow  = (props) => {
+  const product = props.product;
   return (
     <tr>
-      <td>Jill</td>
-      <td>Smith</td>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
     </tr>
   );
 }
+
+const PRODUCTS = [
+  {category :'food', name:'football' , price : '51.99'},
+  {category :'food', name:'basketball' , price : '11.99'},
+  {category :'food', name:'baseball' , price : '31.99'},
+  {category :'beverage',name:'coke' , price : '1.99'},
+  {category :'beverage', name:'tealive' , price : '2.99'},
+  {category :'beverage', name:'latte' , price : '3.99'}
+]
 
 ReactDOM.render(<FilterableProductTable/>, document.getElementById('root'));
